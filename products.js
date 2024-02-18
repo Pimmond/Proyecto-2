09df4aad8cc442b3b16090321d4873748e97c23f
Pimmond <147411712+Pimmond@users.noreply.github.com>
2024-02-18 09:37:10 +0100

    Desarrollo domingo 18 feb

---
+++ products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    name: 'Callaway Paradym',
+    price: 550,
+    stars: 4,
+    reviews: 250,
+    seller: 'GolfShot',
+    image: 'url_callaway_paradym'
+  },
+  {
+    name: 'Titleist Pro V1',
+    price: 60,
+    stars: 5,
+    reviews: 300,
+    seller: 'GolfGalaxy',
+    image: 'url_titleist_pro_v1'
+  },
+  {
+    name: 'TaylorMade SIM Max',
+    price: 450,
+    stars: 4.5,
+    reviews: 200,
+    seller: 'PGA Tour Superstore',
+    image: 'url_taylormade_sim_max'
+  },
+  {
+    name: 'Ping G410 Driver',
+    price: 400,
+    stars: 4.7,
+    reviews: 180,
+    seller: 'GolfDiscount',
+    image: 'url_ping_g410_driver'
+  },
+  {
+    name: 'Cobra King F9 Irons',
+    price: 700,
+    stars: 4.3,
+    reviews: 150,
+    seller: 'GlobalGolf',
+    image: 'url_cobra_king_f9_irons'
+  },
+  {
+    name: 'Mizuno JPX919 Wedge',
+    price: 120,
+    stars: 4.8,
+    reviews: 220,
+    seller: 'RockBottomGolf',
+    image: 'url_mizuno_jpx919_wedge'
+  },
+  {
+    name: 'Odyssey Stroke Lab Putter',
+    price: 200,
+    stars: 4.6,
+    reviews: 170,
+    seller: "Dick's Sporting Goods",
+    image: 'url_odyssey_stroke_lab_putter'
+  },
+  {
+    name: 'Srixon Soft Feel Golf Balls',
+    price: 25,
+    stars: 4.9,
+    reviews: 280,
+    seller: 'Amazon',
+    image: 'url_srixon_soft_feel_golf_balls'
+  },
+  {
+    name: 'Nike Vapor Fly Pro Hybrid',
+    price: 180,
+    stars: 4.4,
+    reviews: 190,
+    seller: 'Golfsmith',
+    image: 'url_nike_vapor_fly_pro_hybrid'
+  },
+  {
+    name: 'Titleist Scotty Cameron',
+    price: 300,
+    stars: 4.2,
+    reviews: 120,
+    seller: 'Vice Pro Shop',
+    image: 'url_titleist_scotty_cameron'
+  }
+]
